4fd69311ca2f8e36f60f2d1d3d2610306fc8a790
bandjeo <branislav.andjelic@gmail.com>
2021-01-27 22:39:49 +0100

    added tfidf to webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, send_from_directory
+import webapp.services.tfidf_service as tfidf_service
+from webapp.services.common import find_law_title
 app = Flask(__name__,  static_url_path='')
 
 @app.route('/')
@@ -9,5 +11,19 @@ def hello_world():
 def send_js(path):
     return send_from_directory('public', path)
 
+@app.route('/tfidf/<query>')
+def tfidf(query):
+    file_names = tfidf_service.sorch([query])
+    data = []
+    for file_name in file_names:
+        title, sample = find_law_title(file_name)
+        data.append({
+            'fileName': file_name,
+            'title': title,
+            'sample': sample
+        })
+
+    return { 'data': data}
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
