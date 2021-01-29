c6ab51461f32846db21ffd5fd66306945abc67e1
Nenad Misic <nenad.misic997@gmail.com>
2021-01-30 00:28:40 +0100

    Changes changes

--- webapp/public/js/index.js
+++ webapp/public/js/index.js
@@ -6,8 +6,16 @@ var algorithms = {
     "5": "Elastic search"
 }
 
-var showResults = () => {
+var showResults = (data) => {
     document.getElementById('no-results').style.display = 'none';
+
+    document.getElementById('result-container').innerHTML = data.map(e => `
+                    <div class="mb-4 col-11 offset-1">
+                        <div class="col-12 d-block text-muted small">/public/acts/${e.fileName}</div>
+                        <a class="col-12 mt-1 d-block h4 cursor-pointer" target="_blank" href="/public/acts/${e.fileName}" >${e.title}</a>
+                        <p class="col-5 d-block">${e.sample}...</p>    
+                    </div>
+    `).join('\n')
     document.getElementById('results').style.display = 'block';
 }
 
@@ -34,8 +42,9 @@ document.getElementById('search-btn').addEventListener('click', (ev) => {
     // alert(`
     // Searched term: ${document.getElementById('searchInput').value}
     // Algorithm type: ${algorithms[getAlg()]}`);
-    axios.get(`${algorithms[getAlg()]}${document.getElementById('searchInput').value}`).then(e => alert(JSON.stringify(e.data, null, 2)))
-    showResults();
+    axios.get(`${algorithms[getAlg()]}${document.getElementById('searchInput').value}`).then(e => {
+        showResults(e.data.data);
+    })
 
 })
 
